74b83f15597e29497efea59fc23c545b19fcc56a
Jerome Biotidara <jbiotid@us.ibm.com>
2018-01-10 14:29:24 -0500

    enabled the continuous running of CR

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
- console.log("hey jay 1")
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+var portNumber = 9001
+app.listen(portNumber, () => console.log('Example app listening on port '+ portNumber + '!'))
