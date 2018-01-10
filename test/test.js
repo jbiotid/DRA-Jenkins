5be3940292fb3bd4a610c927c6774a332ce7f521
Jerome Biotidara <jbiotid@us.ibm.com>
2018-01-10 13:13:07 -0500

    testing polling

--- test/test.js
+++ test/test.js
@@ -2,6 +2,31 @@ var request = require('request'),
 assert = require('assert'),
 base_url = 'http://www.google.com/'
 
+
+var mocha = require('mocha');
+module.exports = MyReporter;
+
+function MyReporter(runner) {
+  mocha.reporters.Base.call(this, runner);
+  var passes = 0;
+  var failures = 0;
+
+  runner.on('pass', function(test){
+    passes++;
+    console.log('pass: %s', test.fullTitle());
+  });
+
+  runner.on('fail', function(test, err){
+    failures++;
+    console.log('fail: %s -- error: %s', test.fullTitle(), err.message);
+  });
+
+  runner.on('end', function(){
+    console.log('end: %d/%d', passes, passes + failures);
+    process.exit(failures);
+  });
+}
+
 describe('Hello World Server', function() {
 	describe('GET /', function() {
 		it('returns status code 200', function(done) {
