a18137afa58dcdde8ec704b7008f0a85dd675536
Jerome Biotidara <jbiotidara@gmail.com>
2018-01-29 12:09:33 -0500

    Will pass

--- test/test.js
+++ test/test.js
@@ -4,35 +4,35 @@ base_url = 'https://free-style-hello-world-v1.mybluemix.net/'
 
 
 var mocha = require('mocha');
-module.exports = MyReporter;
+// module.exports = MyReporter;
 
-function MyReporter(runner) {
-  mocha.reporters.Base.call(this, runner);
-  var passes = 0;
-  var failures = 0;
+// function MyReporter(runner) {
+//   mocha.reporters.Base.call(this, runner);
+//   var passes = 0;
+//   var failures = 0;
 
-  runner.on('pass', function(test){
-    passes++;
-    console.log('pass: %s', test.fullTitle());
-  });
+//   runner.on('pass', function(test){
+//     passes++;
+//     console.log('pass: %s', test.fullTitle());
+//   });
 
-  runner.on('fail', function(test, err){
-    failures++;
-    console.log('fail: %s -- error: %s', test.fullTitle(), err.message);
-  });
+//   runner.on('fail', function(test, err){
+//     failures++;
+//     console.log('fail: %s -- error: %s', test.fullTitle(), err.message);
+//   });
 
-  runner.on('end', function(){
-    console.log('end: %d/%d', passes, passes + failures);
-    process.exit(failures);
-  });
-}
+//   runner.on('end', function(){
+//     console.log('end: %d/%d', passes, passes + failures);
+//     process.exit(failures);
+//   });
+// }
 
 describe('Hello World Server', function() {
 	describe('GET /', function() {
-		it('returns status code 400', function(done) {
+		it('returns status code 200', function(done) {
 			request.get(base_url, function(error, response, body) {
 				//expect(response.statusCode).toBe(200);
-				assert.equal(400, response.statusCode)
+				assert.equal(200, response.statusCode)
 				done()
 			})
 		})
